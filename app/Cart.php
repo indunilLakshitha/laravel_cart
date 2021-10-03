1e2cb6303232ba9a27d3045d9d73f83fc41f74d2
Dasun Tharanga <dasuntharangaflash@gmail.com>
2021-10-03 12:12:58 +0530

    Added $fillable property with column names.

--- app/Cart.php
+++ app/Cart.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
 {
-    //
+    protected $fillable = [
+        'customer_id',
+        'product_id',
+        'qty',
+        'total',
+        'status'
+    ];
 }
